1a54b8596d76091ea0ba3a7531491dc83c222f50
Hugo Fabricio <hugo@brzdigital.com.br>
2017-08-05 13:19:54 -0300

    Bug fixes and add eslint.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,16 +5,16 @@ const availableModules = { Common };
 window.modules = {};
 
 $(() => {
-    const htmlModules = $('[data-module]');
+  const htmlModules = $('[data-module]');
 
     // Loading htmlModules if they are in availableModules
-    htmlModules.each((key, value) => {
-        const mod = $(value).data('module');
+  htmlModules.each((key, value) => {
+    const mod = $(value).data('module');
 
-        if (Object.prototype.hasOwnProperty.call(availableModules, mod)) {
-            window.modules[mod] = new availableModules[mod]();
-        } else {
-            console.log(`The module "${mod}" does not exists.`);
-        }
-    });
+    if (Object.prototype.hasOwnProperty.call(availableModules, mod)) {
+      window.modules[mod] = new availableModules[mod]();
+    } else {
+      console.log(`The module "${mod}" does not exists.`);
+    }
+  });
 });
--- src/scripts/modules/common.js
+++ src/scripts/modules/common.js
@@ -2,31 +2,31 @@ import Mask from './mask';
 import Slide from './slide';
 
 class Common {
-    constructor() {
-        console.log('>>> Common constructor');
+  constructor() {
+    console.log('>>> Common constructor');
 
         // Instance imports
-        new Mask();
-        new Slide();
+    new Mask();
+    new Slide();
 
         // Call methods
-        this.fixedI10();
-        this.disableZoomGesture();
-    }
+    this.fixedI10();
+    this.disableZoomGesture();
+  }
 
-    fixedI10() {
-        if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-            const msViewportStyle = document.createElement('style');
-            msViewportStyle.appendChild(document.createTextNode('@-ms-viewport{width:auto!important}'));
-            document.querySelector('head').appendChild(msViewportStyle);
-        }
+  fixedI10() {
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+      const msViewportStyle = document.createElement('style');
+      msViewportStyle.appendChild(document.createTextNode('@-ms-viewport{width:auto!important}'));
+      document.querySelector('head').appendChild(msViewportStyle);
     }
+  }
 
-    disableZoomGesture() {
-        document.addEventListener('gesturestart', function (e) {
-            e.preventDefault();
-        });
-    }
+  disableZoomGesture() {
+    document.addEventListener('gesturestart', (e) => {
+      e.preventDefault();
+    });
+  }
 }
 
 export default Common;
