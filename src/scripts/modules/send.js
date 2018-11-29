4f59603ad80da3cc827b90bec099b30ccbdb3048
Hugo Fabricio <hugo@brzdigital.com.br>
2018-11-29 13:45:13 -0300

    Init frontend

--- src/scripts/modules/send.js
+++ src/scripts/modules/send.js
@@ -27,11 +27,24 @@ class Send {
     }
 
     for (let i = 0; i < form.length; ++i) {
-      formData.append(form[i].name, form[i].value);
+      if (this.checkType(form[i], 'checkbox') == true || this.checkType(form[i], 'radio') == true) {
+        if (form[i].checked) {
+          formData.append(form[i].name, form[i].value);
+        }
+      } else if (this.checkType(form[i], 'file') == true) {
+        if (form[i].value.length) {
+          formData.append(form[i].name, form[i].files[0], form[i].files[0].name);
+        }
+      } else {
+        formData.append(form[i].name, form[i].value);
+      }
     }
 
     axios({
       method: 'post',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
       url: formAction,
       data: formData
     })
@@ -43,29 +56,36 @@ class Send {
   }
 
   handleResponse(response, form) {
+    let timeout = 0;
+
     // Remove validations
     this.removeValidation(form);
 
-    // Hide modal
-    $('.modal').modal('hide');
-
     // Reset form
-    form.reset();
+    if (typeof response.resetForm == 'undefined'){
+      form.reset();
+    }
 
     // Show alert
-    swal({
-      title: response.title,
-      text: response.message,
-      icon: 'success',
-      timer: 3000,
-      buttons: false
-    });
+    if(typeof response.title !== 'undefined') {
+      swal({
+        title: response.title,
+        text: response.message,
+        icon: 'success',
+        timer: 3000,
+        buttons: false
+      });
+
+      timeout = 2500;
+    } else {
+      timeout = 100;
+    }
 
     // Redirect page
     if(typeof response.redirect !== 'undefined') {
       setTimeout(function(){
         window.location.replace(response.redirect);
-      }, 3000);
+      }, timeout);
     }
   }
 
@@ -75,17 +95,30 @@ class Send {
   }
 
   addValidation(form, error) {
+    if(typeof error.title !== 'undefined') {
+      // Show alert
+      swal({
+        title: error.title,
+        text: error.message,
+        icon: 'error',
+        timer: 3000,
+        buttons: false
+      });
+    }
+
     let data = error.data;
+
     for (let key in data) {
-      let input = form.querySelector(`[name^=${key}`);
+      let input = form.querySelector('input[name="' + key + '"]');
+
       let group = input.closest('.form-group');
-          group.classList.add('-error');
+          group.classList.add('-wrong');
 
-      let label = document.createElement('span');
-          label.classList.add('help-block');
-          label.innerHTML = data[key];
+      let messageError = document.createElement('span');
+          messageError.classList.add('help-block');
+          messageError.innerHTML = data[key];
 
-      group.appendChild(label);
+      group.appendChild(messageError);
     }
   }
 
@@ -93,9 +126,13 @@ class Send {
     let groups = form.querySelectorAll('.form-group');
 
     Array.from(groups).forEach((group) => {
-      if (group.classList.contains('-error')) group.classList.remove('-error');
+      if (group.classList.contains('-wrong')) group.classList.remove('-wrong');
       if (group.querySelector('.help-block')) group.removeChild(group.querySelector('.help-block'));
     });
+  }
+
+  checkType(element, type) {
+    return element instanceof HTMLInputElement && element.getAttribute('type') == type;
   }
 }
 
